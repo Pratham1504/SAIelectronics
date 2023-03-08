34739d2d4c7b685edc2cbe3341dfb3042ab6101c
Pratham goyal <95137153+Pratham1504@users.noreply.github.com>
2023-03-08 19:21:15 +0530

    filtered page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+<<<<<<< Updated upstream
 import {BrowserRouter , Routes , Route  } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
 import Services from './pages/Services';
+=======
+import Filtered from './pages/filtered';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
       <BrowserRouter>
                 <div className="pages">
                     <Routes>
@@ -26,6 +31,9 @@ function App() {
                     </Routes>
                 </div>
             </BrowserRouter>
+=======
+      <Filtered/>
+>>>>>>> Stashed changes
     </>
   );
 }
