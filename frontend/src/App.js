5c9cba540468562cf6c2a944e6da52e80fc3a4a2
Pratham goyal <95137153+Pratham1504@users.noreply.github.com>
2023-03-08 19:28:14 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,10 @@
 import './App.css';
-<<<<<<< Updated upstream
-import {BrowserRouter , Routes , Route  } from 'react-router-dom';
-
-// pages
-import Home from './pages/Home';
-import Services from './pages/Services';
-=======
 import Filtered from './pages/filtered';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
-      <BrowserRouter>
-                <div className="pages">
-                    <Routes>
-                        <Route
-                            path='/'
-                            element= {<Home/>}
-                        />
-                        <Route
-                            path='/services'
-                            element= {<Services/>}
-                        />
-                        {/* <Route
-                            path='/companies'
-                            element= {<Companies/>}
-                        /> */}
-                    </Routes>
-                </div>
-            </BrowserRouter>
-=======
       <Filtered/>
->>>>>>> Stashed changes
     </>
   );
 }
